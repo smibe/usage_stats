abdff88d149e780257d12e478bae05850deff613
Gerhard <Gerhard.Herbert@avira.com>
2018-01-18 17:42:34 +0100

    Added package name.

--- lib/usage_stats.dart
+++ lib/usage_stats.dart
@@ -27,6 +27,35 @@ class UsageStatsData
 }
 
 
+class EventData
+{
+  String packageName;
+  String appName;
+  int timestamp;
+  int eventType;
+
+  static const int  MOVE_TO_FOREGROUND = 1;
+  static const int  MOVE_TO_BACKGROUND = 2;
+  static const int CONFIGURATION_CHANGE = 5;
+
+  EventData(this.timestamp, this.eventType, this.packageName, this.appName, );
+
+  EventData.fromCsv(String csvString) {
+  
+    var entry = csvString.split(";");
+    if (entry.length == 4) {
+      this.timestamp = int.parse(entry[0]);
+      this.eventType = int.parse(entry[1]);
+      this.packageName = entry[2];
+      this.appName = entry[3];
+
+      if (this.appName == "(unknown)")
+        this.appName = entry[1];
+    }
+  }
+}
+
+
 class UsageStats {
   static const MethodChannel _channel =
       const MethodChannel('usage_stats');
@@ -40,5 +69,62 @@ class UsageStats {
       args["end"] = end;
       List<String> list =  await _channel.invokeMethod('usageStats', args);
       return list.map((s) => new UsageStatsData.fromCsv(s)).toList();
+  }
+
+  static Future<List<EventData>> getEvents(int start, int end) async {
+      dynamic args = new Map();
+      args["start"] = start;
+      args["end"] = end;
+      List<String> list =  await _channel.invokeMethod('getEvents', args);
+      return list.map((s) => new EventData.fromCsv(s)).toList();
+  }
+
+  static void updateUsageData(Map<String, UsageStatsData> usages, EventData event, String packageName, int timestamp)
+  {
+    if (usages.containsKey(packageName))
+    {
+      var usageData = usages[packageName];
+      usageData.duration += event.timestamp - timestamp;
+    }
+    else 
+    {
+      var usageData = new UsageStatsData(packageName, event.appName, 0);
+      usageData.duration += event.timestamp - timestamp;
+      usages[packageName] = usageData;
+    }
+  }
+
+  static Future<List<UsageStatsData>>buildUsageStats(int start, int end) async
+  {
+    var events = await getEvents(start, end);
+    int timestamp = start;
+    String packageName = "";
+    Map<String, UsageStatsData> usages = new Map<String, UsageStatsData>();
+    for (var event in events)
+    {
+      if (event.eventType  == EventData.MOVE_TO_BACKGROUND && packageName == event.packageName)
+      {
+        updateUsageData(usages, event, packageName, timestamp);
+        timestamp = event.timestamp;
+        packageName = "";
+      }
+
+      if (event.eventType  == EventData.MOVE_TO_FOREGROUND)
+      {
+        if (packageName != "")
+        {
+          updateUsageData(usages, event, packageName, timestamp);
+        }
+        timestamp = event.timestamp;
+        packageName = event.packageName;
+      }
+    }
+
+    //update the last package
+    if (packageName != "")
+    {
+      updateUsageData(usages, event, packageName, timestamp);
+    }
+return  usages.values.toList();
   }
 }

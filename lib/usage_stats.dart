bc30618c5cb7713533b50daceafd1cfe656bde24
Gerhard Herbert <gerhard.herbert@smibe.com>
2018-02-03 07:11:40 +0100

    Moved usageStatsData into a separate file.

--- lib/usage_stats.dart
+++ lib/usage_stats.dart
@@ -1,31 +1,6 @@
-    import 'dart:async';
-
+import 'dart:async';
 import 'package:flutter/services.dart';
-
-class UsageStatsData
-{
-  String packageName;
-  String appName;
-  int duration;
-
-  UsageStatsData(this.packageName, this.appName, this.duration);
-
-  UsageStatsData.fromCsv(String csvString) {
-  
-    var entry = csvString.split(";");
-    if (entry.length >= 2) {
-      this.duration = int.parse(entry[0]);
-      this.packageName = entry[1];
-    }
-
-    if (entry.length >= 3) {
-      this.appName = entry[2];
-      if (this.appName == "(unknown)")
-        this.appName = entry[1];
-    }
-  }
-}
-
+import 'usage_stats_data.dart';
 
 class UsageStats {
   static const MethodChannel _channel =
